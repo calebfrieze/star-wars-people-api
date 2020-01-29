d8e87fbbe966c239b01a4de50662ca21e7f42fb0
Caleb Frieze <calebfrieze@gmail.com>
2020-01-29 13:20:33 -0700

    add try catch

--- src/api/People/controllers/deletePerson.controller.ts
+++ src/api/People/controllers/deletePerson.controller.ts
@@ -4,11 +4,17 @@ import deletePerson from "../services/deletePerson.service";
 const deletePersonController = async (req: Request, res: Response) => {
   const { id } = req.params;
 
-  await deletePerson(Number(id));
+  try {
+    await deletePerson(Number(id));
 
-  return res.status(200).json({
-    message: `Person: ${id} deleted`
-  });
+    return res.status(200).json({
+      message: `Person: ${id} deleted`
+    });
+  } catch (e) {
+    return res.status(500).json({
+      error: e.message
+    });
+  }
 }
 
 export default deletePersonController;
