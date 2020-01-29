344ef19326ae23738026fe7e9fba1019ca0d3aa4
Caleb Frieze <calebfrieze@gmail.com>
2020-01-29 13:20:39 -0700

    allow cross origin

--- src/server.ts
+++ src/server.ts
@@ -12,6 +12,12 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use("/people", People);
 
 app.use("/refresh-people", refreshPeople)
